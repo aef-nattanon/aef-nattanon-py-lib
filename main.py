ca42737baed7c839fdab3582d8602062a15b1207
aef-nattanon <48983118+aef-nattanon@users.noreply.github.com>
2021-09-27 17:39:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 # Import Multiplication from your library
-
-from aef_nattanon_py.multiplication import Multiplication
-from aef_nattanon_py.photoExample import PhotoExample
+import cv2
+import aef_nattanon_py as aef
 
 if __name__ == '__main__':
-    byExample = PhotoExample('coco.names', 'yolov3-tiny.cfg', 'yolov3-tiny.weights')
-    byExample.show_by_photo('my-photo.jpeg')
-    # Instantiate a Multiplication object
-    multiplication = Multiplication(2)
+    # YoloV3Detector
+    myDetector = aef.Detector(
+        'aef_nattanon_py/files/coco.names',
+        'aef_nattanon_py/files/yolov3-tiny.cfg',
+        'aef_nattanon_py/files/yolov3-tiny.weights'
+    )
+    myDetector.detect_by_img_path('./my-photo.jpeg')
+    cv2.imshow("Image", myDetector.detect_img)
+    cv2.waitKey(0)
 
-    # Call the multiply method
+    # YoloV3Detector
+    myDetector = aef.YoloV3Detector()
+    myDetector.detect_by_img_path('./my-photo.jpeg')
+    cv2.imshow("Image", myDetector.detect_img)
+    cv2.waitKey(0)
+
+    # Multiplication
+    multiplication = aef.Multiplication(2)
     print(multiplication.multiply(5))
